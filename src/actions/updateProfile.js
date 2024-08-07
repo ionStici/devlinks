3c6d064ae8b1f45e763a89bda8fcc98115076ce1
Ion Stici <ionsticidev@gmail.com>
2024-08-07 09:30:47 +0300

    Updates

--- src/actions/updateProfile.js
+++ src/actions/updateProfile.js
@@ -8,10 +8,7 @@ import { emailRegex, nameRegex } from "@/utils/regex";
 
 async function deleteImage(path) {
   const supabase = createClient();
-
-  const { error } = await supabase.storage.from("avatars").remove([path]);
-
-  if (error) throw new Error(error.message);
+  await supabase.storage.from("avatars").remove([path]);
 }
 
 // // // // // // // // // // // // // // // // // // // //
@@ -19,11 +16,13 @@ async function deleteImage(path) {
 export async function updateProfile(formData) {
   const supabase = createClient();
 
+  // Prepare data
   const image = formData.get("picture");
   const firstName = formData.get("firstName");
   const lastName = formData.get("lastName");
   const email = formData.get("email");
 
+  // Input validation
   if (!emailRegex.test(email)) throw new Error("Invalid Email");
   if (!nameRegex.test(firstName)) throw new Error("Invalid First Name");
   if (!nameRegex.test(lastName)) throw new Error("Invalid Last Name");
@@ -32,8 +31,7 @@ export async function updateProfile(formData) {
     throw new Error("Wrong image format");
   }
 
-  return;
-
+  // Update data
   const { data, error } = await supabase.auth.updateUser({
     email,
     data: { firstName, lastName },
@@ -41,24 +39,29 @@ export async function updateProfile(formData) {
 
   if (error) throw new Error(error.message);
 
-  const { user } = data;
+  // Upload Image
   const currentImage = data.user.user_metadata.image;
-  if (image && currentImage) await deleteImage(currentImage);
+  if (image.size > 0 && currentImage) {
+    await deleteImage(currentImage);
 
-  if (image) {
-    const { data, error } = await supabase.storage
+    const imageName = `${Math.random()}-${image.name}`
+      .replace("/", "")
+      .slice(2);
+
+    const { data: imgData, error: imgError } = await supabase.storage
       .from("avatars")
-      .upload(user.email.split("@")[0], image, {
-        cacheControl: "3600",
-        upsert: false,
-      });
-    if (error) throw new Error(error.message);
+      .upload(imageName, image, { cacheControl: "3600", upsert: false });
 
-    await supabase.auth.updateUser({
-      data: { image: data.path },
+    if (imgError) throw new Error(imgError.message);
+
+    const { error: storePathError } = await supabase.auth.updateUser({
+      data: { image: imgData.path },
     });
+
+    if (storePathError) throw new Error(storePathError.message);
   }
 
+  // Revalidate Path
   revalidatePath("/edit/profile");
 }
 
