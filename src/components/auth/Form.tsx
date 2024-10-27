9bf6c93a76d62e1e2b7f61a2ab44c6e8179420d7
Ion Stici <ionsticidev@gmail.com>
2024-10-27 09:15:30 +0200

    Starting the project from scratch & Change the tech stack to React + NestJS

--- src/components/auth/Form.tsx
+++
@@ -1,9 +0,0 @@
-import { type ReactNode } from "react";
-
-type FormProps = {
-  children: ReactNode;
-};
-
-export default function Form({ children }: FormProps) {
-  return <form className="flex flex-col gap-6 mb-6">{children}</form>;
-}
--- src/utils/constants.ts
+++
@@ -1,1 +0,0 @@
-export const URL = "https://devlinks.fun";
